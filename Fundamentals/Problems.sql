64e8f21d177124d4d553788c8134c82011a384c0
Muhammad H. Bakr <162805518+Muhammad-H-Bakr@users.noreply.github.com>
2024-03-17 18:38:35 +0200

    Fundamentals of PL/SQL + Good topics in SQL.

---
+++ Fundamentals/Problems.sql
@@ -0,0 +1,274 @@
+-- Write a PL/SQL block that checks whether a given number from the user is prime.
+DECLARE
+    USER_INPUT   NUMBER := &NUMBER;
+    LOOP_COUNTER NUMBER := USER_INPUT-1;
+    FLAG         NUMBER := 0;
+BEGIN
+    IF USER_INPUT > 2 THEN
+        LOOP
+            IF USER_INPUT MOD LOOP_COUNTER = 0 THEN
+                FLAG := 1;
+            END IF;
+
+            LOOP_COUNTER := LOOP_COUNTER - 1;
+            EXIT WHEN LOOP_COUNTER = 1 OR FLAG = 1;
+        END LOOP;
+    ELSIF USER_INPUT <= 1 THEN
+        FLAG := 1;
+    END IF;
+
+    IF FLAG = 0 THEN
+        DBMS_OUTPUT.PUT_LINE('Prime.');
+    ELSIF FLAG = 1 THEN
+        DBMS_OUTPUT.PUT_LINE('Not Prime.');
+    END IF;
+END;
+/
+
+/* Write a PL/SQL block that prints the largest and smallest in an array.
+type arr is varray(5) of number;
+myArr arr := arr(10,4,6,22,11); */
+DECLARE
+    TYPE ARR IS
+        VARRAY(5) OF NUMBER;
+    MYARR ARR := ARR(10, 4, 6, 22, 11);
+    MN    NUMBER := MYARR(1);
+    MX    NUMBER := 0;
+BEGIN
+    FOR I IN 1..MYARR.COUNT LOOP
+        IF MYARR(I) > MX THEN
+            MX := MYARR(I);
+        END IF;
+
+        IF MYARR(I) < MN THEN
+            MN := MYARR(I);
+        END IF;
+    END LOOP;
+
+    DBMS_OUTPUT.PUT_LINE('Max: '
+                         ||MX
+                         ||', Min: '
+                         ||MN);
+END;
+/
+
+/* Write a PL/SQL block that prints every number and its frequency in an array.
+Eg: type arr is varray(6) of number;
+myArr arr := arr(10,10,10,6,6,11); */
+DECLARE
+    TYPE ARR IS
+        VARRAY(6) OF NUMBER;
+    MYARR   ARR := ARR(10, 10, 10, 6, 6, 11);
+    TYPE HELPER IS
+        TABLE OF NUMBER INDEX BY PLS_INTEGER;
+    FREQ    HELPER;
+    COUNTER NUMBER;
+BEGIN
+    FOR I IN 1..MYARR.COUNT LOOP
+        IF NOT (FREQ.EXISTS(MYARR(I))) THEN
+            FREQ(MYARR(I)) := 1;
+        ELSE
+            FREQ(MYARR(I)) := FREQ(MYARR(I)) + 1;
+        END IF;
+    END LOOP;
+
+    FOR I IN FREQ.FIRST .. FREQ.LAST LOOP
+        IF FREQ.EXISTS(I) THEN
+            IF FREQ(I) = 1 THEN
+                DBMS_OUTPUT.PUT_LINE('The number '
+                                     ||I
+                                     ||' appears one time.');
+            ELSE
+                DBMS_OUTPUT.PUT_LINE('The number '
+                                     ||I
+                                     ||' appears '
+                                     ||FREQ(I)
+                                     ||' times.');
+            END IF;
+        END IF;
+    END LOOP;
+END;
+/
+
+-- Write a PL/SQL block that takes a number n from the user and outputs the nth term in Fibonacci's sequence.
+DECLARE
+    USER_INPUT NUMBER := &VAL;
+    PREV       NUMBER := 0;
+    MID        NUMBER;
+    CURR       NUMBER := 1;
+BEGIN
+    IF(USER_INPUT = 0) THEN
+        DBMS_OUTPUT.PUT_LINE(PREV);
+    ELSIF(USER_INPUT = 1) THEN
+        DBMS_OUTPUT.PUT_LINE(CURR);
+    ELSE
+        FOR I IN 2..USER_INPUT LOOP
+            MID := PREV;
+            PREV := CURR;
+            CURR := CURR + MID;
+        END LOOP;
+
+        DBMS_OUTPUT.PUT_LINE(CURR);
+    END IF;
+END;
+/
+
+-- Write a PL/SQL block to print the first_name and the salary for all the employees using explicit cursors.
+DECLARE
+    CURSOR CUR IS (
+        SELECT
+            FIRST_NAME,
+            SALARY
+        FROM
+            EMPLOYEES
+    );
+BEGIN
+    FOR REC IN CUR LOOP
+        DBMS_OUTPUT.PUT_LINE('Name: '
+                             ||REC.FIRST_NAME
+                             ||', salary: '
+                             ||REC.SALARY);
+    END LOOP;
+END;
+/
+
+/* The user is prompted to enter a date in the past.
+Write a PL/SQL that prints the number of employees who are hired after the specified date.
+When the given date is greater than or equal to the current date an exception should be triggered...
+printing the following message “invalid date”. */
+DECLARE
+    USER_INPUT DATE := '&VAL';
+    OUTPUT     NUMBER;
+BEGIN
+    IF USER_INPUT >= TO_DATE(SYSDATE) THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Invalid Date.');
+    ELSE
+        SELECT
+            COUNT(*) INTO OUTPUT
+        FROM
+            EMPLOYEES
+        WHERE
+            HIRE_DATE > USER_INPUT;
+        DBMS_OUTPUT.PUT_LINE('#Of Employees: '
+                             ||OUTPUT
+                             ||'.');
+    END IF;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE(SQLERRM);
+END;
+/
+
+-- First Hard SQL Problem on LeetCode: https://leetcode.com/problems/department-top-three-salaries/description/.
+SELECT
+    DEP_NAME "Department",
+    EMP_NAME "Employee",
+    SALARY   "Salary"
+FROM
+    (
+        SELECT
+            SALARY,
+            D.NAME                                                        AS DEP_NAME,
+            E.NAME                                                        AS EMP_NAME,
+            DENSE_RANK() OVER(PARTITION BY D.NAME ORDER BY E.SALARY DESC) AS RANKED
+        FROM
+            EMPLOYEE   E
+            JOIN DEPARTMENT D
+            ON (E.DEPARTMENTID = D.ID)
+    )
+WHERE
+    RANKED <= 3
+ORDER BY
+    3 DESC;
+
+-- Second Hard SQL Problem on LeetCode: https://leetcode.com/problems/trips-and-users/description/.
+SELECT
+    REQUEST_AT                                                    "Day",
+    ROUND(1-(SUM(DECODE(STATUS, 'completed', 1, 0))/COUNT(*)), 2) "Cancellation Rate"
+FROM
+    TRIPS
+WHERE
+    CLIENT_ID NOT IN (
+        SELECT
+            USERS_ID
+        FROM
+            USERS
+        WHERE
+            BANNED = 'Yes'
+    )
+    AND DRIVER_ID NOT IN (
+        SELECT
+            USERS_ID
+        FROM
+            USERS
+        WHERE
+            BANNED = 'Yes'
+    )
+    AND REQUEST_AT BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY
+    REQUEST_AT
+ORDER BY
+    1;
+
+SELECT
+    ID,
+    VISIT_DATE,
+    PEOPLE
+FROM
+    (
+        SELECT
+            ID,
+            VISIT_DATE,
+            PEOPLE LAG(ID) OVER (ORDER BY ID) AS PREV_VALUE
+        FROM
+            STADIUM
+        WHERE
+            PEOPLE >= 100
+    )
+WHERE
+    PREV_VALUE = ID - 1;
+
+-- Third Hard SQL Problem on LeetCode: https://leetcode.com/problems/human-traffic-of-stadium/description/.
+WITH CTE AS (
+    SELECT
+        GRP
+    FROM
+        (
+            SELECT
+                ID - ROW_NUMBER() OVER (ORDER BY ID) AS GRP
+            FROM
+                STADIUM
+            WHERE
+                PEOPLE >= 100
+        )
+    GROUP BY
+        GRP
+    HAVING
+        COUNT(GRP) >= 3
+)
+SELECT
+    ID                  "id",
+    TO_CHAR(VISIT_DATE) "visit_date",
+    PEOPLE              "people"
+FROM
+    (
+        SELECT
+            ID,
+            VISIT_DATE,
+            PEOPLE,
+            ID - ROW_NUMBER() OVER (ORDER BY ID) AS QQ
+        FROM
+            STADIUM
+        WHERE
+            PEOPLE >= 100
+    )
+WHERE
+    QQ IN (
+        SELECT
+            GRP
+        FROM
+            CTE1
+    )
+ORDER BY
+    1;
+
