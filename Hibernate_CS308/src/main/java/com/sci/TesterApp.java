0bdb7096c8f59d84d65174db678446fc640df85c
#!Roger!? 🐺 <162805518+Muhammad-H-Bakr@users.noreply.github.com>
2024-04-30 18:54:40 +0300

    Query With Filter Testing.

--- Hibernate_CS308/src/main/java/com/sci/TesterApp.java
+++ Hibernate_CS308/src/main/java/com/sci/TesterApp.java
@@ -1,34 +1,34 @@
 package com.sci;
 
+import com.sci.criteria.FilterQuery;
+import com.sci.criteria.Operator;
 import com.sci.dao.DBConfig;
 import com.sci.dao.DBManagement;
 import com.sci.models.*;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TesterApp {
     public static void main(String[] args) {
-        DBManagement.DBPerson emp = new DBManagement.DBPerson();
-//        emp.create(new Person(1,"Ahmed"));
-//        emp.create(new Person(2,"Mohammed"));
-//        emp.delete(2);
-        List<Person> employees = emp.get();
-        for (Person employee : employees) {
-            //WHEN USING BI-DIRECTIONAL RELATIONS: DO NOT PRINT THE WHOLE OBJECT (DEADLOCK).
-            System.out.print("Person: "+ employee.getName()+"\nCars: ");
-            for(Car car : employee.getCars()) {
-                System.out.print(car.getName()+" ");
-            }
-            System.out.println();
+        DBManagement.DBEmployee emp = new DBManagement.DBEmployee();
+        List<FilterQuery> filters = new ArrayList<>();
+        Date hireDate = Date.valueOf("2010-01-01"); //YYYY-MM-DD. (Numeric)
+        filters.add(new FilterQuery("salary", 5000,
+                Operator.GT));
+        filters.add(new FilterQuery("firstName", "Steven",
+                Operator.EQ));
+        filters.add(new FilterQuery("hireDate", hireDate,
+                Operator.LT));
+        filters.add(new FilterQuery("jobId", "AD_PRES",
+                Operator.EQ));
+        List<Employee> emps = emp.getByFilter(filters);
+        System.out.println("EmployeeId, FirstName, LastName" );
+        for (Employee employee : emps) {
+            System.out.println(employee.getEmployeeId() + " " +
+                    employee.getFirstName() + " " + employee.getLastName());
         }
-
-//        DBManagement.DBCar emp = new DBManagement.DBCar();
-//        emp.create(new Car(1,"BMW",1));
-//        List<Car> employees = emp.get();
-//        for (Car employee : employees) {
-//            //WHEN USING BI-DIRECTIONAL RELATIONS: DO NOT PRINT THE WHOLE OBJECT (DEADLOCK).
-//            System.out.println(employee);
-//        }
         DBConfig.shutdown();
     }
 }
